c32d1d00dd63a84fcfb4b93ccb7387c7b6ac9318
AminS <safarin207368@gmail.com>
2025-04-10 12:59:28 +0330

    Initial commit

---
+++ src/main/java/com/contactsapi/ContactsApiApp.java
@@ -0,0 +1,11 @@
+package com.contactsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactsApiApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ContactsApiApp.class, args);
+    }
+}
